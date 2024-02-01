7bcb6599ba574adc36f40dc796efa98f035aa723
Yuristam <yuristamasanakunov@gmail.com>
2024-02-01 22:48:25 +0600

    update migration for book entity

---
+++ BlazorBooks.Api/Migrations/20240201164527_InitialUpdateBook.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorBooks.Api.Migrations
+{
+    public partial class InitialUpdateBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Genre_GenreId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_GenreId",
+                table: "Books");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genre",
+                table: "Genre");
+
+            migrationBuilder.RenameTable(
+                name: "Genre",
+                newName: "Genres");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres");
+
+            migrationBuilder.RenameTable(
+                name: "Genres",
+                newName: "Genre");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genre",
+                table: "Genre",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_GenreId",
+                table: "Books",
+                column: "GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Genre_GenreId",
+                table: "Books",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

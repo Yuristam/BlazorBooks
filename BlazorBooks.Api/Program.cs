29c5d664f23cd1fa179272263f2f70e8e9225aa5
Yuristam <yuristamasanakunov@gmail.com>
2024-02-01 21:33:51 +0600

    add migration and sql connection in program.cs

--- BlazorBooks.Api/Program.cs
+++ BlazorBooks.Api/Program.cs
@@ -1,15 +1,18 @@
+using BlazorBooks.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContextPool<BlazorBooksDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BlazorBooksConnection"))
+);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +20,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();

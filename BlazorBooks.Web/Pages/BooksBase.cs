47c7cf66b7713e6595c0fb05df1ab0717b2618fb
Yuristam <yuristamasanakunov@gmail.com>
2024-02-02 21:04:00 +0600

    add DisplayBooks

--- BlazorBooks.Web/Pages/BooksBase.cs
+++ BlazorBooks.Web/Pages/BooksBase.cs
@@ -15,5 +15,19 @@ namespace BlazorBooks.Web.Pages
         {
             Books = await BookService.GetBooks();
         }
+
+        protected IOrderedEnumerable<IGrouping<int, BookDto>> GetGroupedBooksByGenre()
+        {
+            return from book in Books
+                   group book by book.GenreId into bookByGenreGroup
+                   orderby bookByGenreGroup.Key
+                   select bookByGenreGroup;
+        }
+
+        protected string GetGenreName(IGrouping<int, BookDto> groupedBookDtos) 
+        {
+            return groupedBookDtos.FirstOrDefault(pg => pg.GenreId == groupedBookDtos.Key).GenreName;
+        }
+
     }
 }
---
+++ BlazorBooks.Web/Pages/DisplayBooksBase.cs
@@ -0,0 +1,11 @@
+﻿using BlazorBooks.Models.Dtos;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorBooks.Web.Pages
+{
+    public class DisplayBooksBase : ComponentBase
+    {
+        [Parameter]
+        public IEnumerable<BookDto> Books { get; set; }
+    }
+}
